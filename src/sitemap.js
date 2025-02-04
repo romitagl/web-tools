650a78c2cb7530b9af1d30ff9c7e8b28e0b4d2a4
Gian Luigi Romita <7878798+romitagl@users.noreply.github.com>
2025-02-04 22:25:27 +0000

    Fix sitemap

---
+++ src/sitemap.js
@@ -0,0 +1,31 @@
+// src/sitemap.js
+import { SitemapStream, streamToPromise } from 'sitemap';
+import { createGzip } from 'zlib';
+import fs from 'fs';
+
+const hostname = 'https://romitagl.github.io/web-tools/';
+
+const urls = [
+  { url: '/web-tools', changefreq: 'daily', priority: 1 },
+  // { url: '/about', changefreq: 'monthly', priority: 0.8 },
+  // { url: '/contact', changefreq: 'monthly', priority: 0.8 },
+  // Add additional routes here
+];
+
+// Create a sitemap stream
+const sitemap = new SitemapStream({ hostname });
+
+// Write the sitemap to a file
+const writeStream = fs.createWriteStream('./public/sitemap.xml');
+sitemap.pipe(writeStream);
+
+// Add URLs to the sitemap
+urls.forEach(url => sitemap.write(url));
+
+// End the stream
+sitemap.end();
+
+// Optionally, compress the sitemap
+streamToPromise(sitemap.pipe(createGzip()))
+  .then(() => console.log('Sitemap created successfully!'))
+  .catch(console.error);
