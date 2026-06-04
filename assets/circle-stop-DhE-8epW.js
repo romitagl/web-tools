526966d9dba753cc67d74357cbe0b680135c4663
romitagl <7878798+romitagl@users.noreply.github.com>
2026-06-04 05:29:40 +0000

    Deploying to gh-pages from @ romitagl/web-tools@a2f710c79bb7b494d9ef19109207ec9434950489 🚀

---
+++ assets/circle-stop-DhE-8epW.js
@@ -0,0 +1,2 @@
+import{n as e}from"./jsx-runtime-DBOrRBQe.js";var t=e(`circle-stop`,[[`circle`,{cx:`12`,cy:`12`,r:`10`,key:`1mglay`}],[`rect`,{x:`9`,y:`9`,width:`6`,height:`6`,rx:`1`,key:`1ssd4o`}]]);export{t};
+//# sourceMappingURL=circle-stop-DhE-8epW.js.map
